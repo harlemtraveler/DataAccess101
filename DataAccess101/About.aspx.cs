5d51162455a60a5e9300bda34cab799ff2469a49
Mark Rosenberg <mrosenberg@prscholas.org>
2021-04-26 14:47:10 -0500

    DataAdapter version

--- DataAccess101/About.aspx.cs
+++ DataAccess101/About.aspx.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Configuration;
+using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
@@ -21,14 +22,22 @@ namespace DataAccess101
                     cmd.Connection = con;
                     cmd.CommandText = "Select * from HumanResources.Department";
                     cmd.CommandType = System.Data.CommandType.Text;
-                    con.Open();
-                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    //con.Open();
+                    //using (SqlDataReader reader = cmd.ExecuteReader())
+                    //{
+                    //    if (reader.Read())
+                    //    {
+                    //        TextBox1.Text = TextBox1.Text + reader.GetValue(0);
+                    //        TextBox2.Text = TextBox2.Text + reader.GetValue(1);
+                    //    }
+                    //}
+                    using (SqlDataAdapter adapter = new SqlDataAdapter())
                     {
-                        if (reader.Read())
-                        {
-                            TextBox1.Text = TextBox1.Text + reader.GetValue(0);
-                            TextBox2.Text = TextBox2.Text + reader.GetValue(1);
-                        }
+                        adapter.SelectCommand = cmd;
+                        DataSet ds = new DataSet();
+                        adapter.Fill(ds);
+                        GridView1.DataSource = ds.Tables[0];
+                        GridView1.DataBind();
                     }
                 }
             }
